1164607836593a37bbb86bd454c38fac918edbb4
HuChao <1396953791@qq.com>
2019-04-08 14:17:04 +0800

    修改支付寶接口問題

--- user_module/src/main/java/cn/javaexception/DemoApplication.java
+++ user_module/src/main/java/cn/javaexception/DemoApplication.java
@@ -3,7 +3,6 @@ package cn.javaexception;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
