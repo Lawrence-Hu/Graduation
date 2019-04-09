97ffc8eae73e82ff3ce4a5d52e87d50f9c7a3c19
敬东润 <673509005@qq.com>
2019-04-09 09:43:17 +0800

    修改添加商品接口，新增IS属性修改接口

--- product_module/src/main/java/cn/javaexception/service/impl/ProductInterfaceImpl.java
+++ product_module/src/main/java/cn/javaexception/service/impl/ProductInterfaceImpl.java
@@ -10,6 +10,7 @@ import product_module.entity.Product;
 import product_module.service.ProductInterface;
 import utils.JsonData;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -33,6 +34,7 @@ public class ProductInterfaceImpl implements ProductInterface {
      * */
     @Override
     public JsonData findProductById(Integer id) {
+
         if(id ==null){
             return  JsonData.buildError("商品id为空！");
         }
@@ -82,10 +84,37 @@ public class ProductInterfaceImpl implements ProductInterface {
     public JsonData addProduct(Product product) {
 
         List<Product.Img> imgList = product.getImg();
-        if (product == null || imgList == null)//验证判断
+        if (product == null ||  imgList == null)//验证判断
         {
-            return JsonData.buildError("请输入商品的信息");
+            return JsonData.buildError("请输入商品的信息!");
         }
+        if(product.getProductName()==null)
+        {
+            return  JsonData.buildError("商品名字不能为空!");
+        }
+        if(product.getMarketPrice()==null||product.getMarketPrice()<0)
+        {
+            return  JsonData.buildError("商品价格不能为空或负数!");
+        }
+        if(product.getProductsStock()==null||product.getProductsStock()<0)
+        {
+            return  JsonData.buildError("商品库存不能为空或负数!");
+        }
+        if(product.getProductUnitId()==null)
+        {
+            return  JsonData.buildError("商品规格不能为空!");
+        }
+
+        if(product.getBrandId()==null)
+        {
+            return  JsonData.buildError("商品品牌不能为空!");
+        }
+        if(product.getProductDesc()==null)
+        {
+            return  JsonData.buildError("商品描述不能为空!");
+        }
+        product.setSaleTime(LocalDateTime.now() );
+
         int i = productMapper.insert(product);
         int a = 0;
         for (Product.Img img : imgList) {
@@ -124,5 +153,77 @@ public class ProductInterfaceImpl implements ProductInterface {
 
         return i+j>0? JsonData.buildSuccess("删除成功"):JsonData.buildError("系统异常，请稍后再试");
 
+    }
+
+    /**
+     * @param id,type
+     * @return JsonData
+     * @author JDR
+     * @description 是否开售
+     */
+    @Override
+    public JsonData updateIsSaleById(Integer id, Boolean type) {
+        if(id==null||type==null)
+        {
+            return  JsonData.buildError("商品id或type不能为空!");
+        }
+        Product product=productMapper.selectById(id);
+        if(product==null)
+        {
+            JsonData.buildError("商品不存在，请检查id是否正确!");
+        }
+        String msg = product.getProductStatus();
+        if(msg=="-1")
+        {
+            JsonData.buildError("商品违规!");
+        }
+        if(msg=="0")
+        {
+            JsonData.buildError("商品未审核!");
+        }
+        //type为真,修改值为1
+        if(product.getIsSale()=="0" && type==true)
+        {
+            productMapper.updateById(product.setIsSale("1"));
+            return JsonData.buildSuccess("修改成功,商品已开售!");
+        }
+        if(product.getIsSale()=="1" && type==false)
+        {
+            productMapper.updateById(product.setIsSale("0"));
+            return  JsonData.buildSuccess("修改成功，商品已停售");
+        }
+        return JsonData.buildError("操作失败，请检查商品状态后重试!");
+    }
+    /**
+     * @param id,type
+     * @return JsonData
+     * @author JDR
+     * @description 是否精品
+     */
+    @Override
+    public JsonData updateIsBestById(Integer id, Boolean type) {
+        return null;
+    }
+
+    /**
+     * @param id,type
+     * @return JsonData
+     * @author JDR
+     * @description 是否热销
+     */
+    @Override
+    public JsonData updateIsNewById(Integer id, Boolean type) {
+        return null;
+    }
+
+    /**
+     * @param id,type
+     * @return JsonData
+     * @author JDR
+     * @description 是否推荐
+     */
+    @Override
+    public JsonData updateIsRecomById(Integer id, Boolean type) {
+        return null;
     }
 }
