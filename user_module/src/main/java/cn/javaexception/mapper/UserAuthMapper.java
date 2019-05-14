24473ea72b3b527cb84610ba836dea9ad2910632
Lawrence-Hu <huchao123>
2019-05-15 00:00:47 +0800

    更新

---
+++ user_module/src/main/java/cn/javaexception/mapper/UserAuthMapper.java
@@ -0,0 +1,18 @@
+package cn.javaexception.mapper;
+
+import cn.javaexception.entity.UserAuth;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author huchao
+ * @since 2019-05-13
+ */
+@Mapper
+public interface UserAuthMapper extends BaseMapper<UserAuth> {
+
+}
---
+++ user_module/src/main/java/cn/javaexception/mapper/UserRoleMapper.java
@@ -0,0 +1,18 @@
+package cn.javaexception.mapper;
+
+import cn.javaexception.entity.UserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author huchao
+ * @since 2019-05-11
+ */
+@Mapper
+public interface UserRoleMapper extends BaseMapper<UserRole> {
+
+}
